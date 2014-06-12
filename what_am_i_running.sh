b7eb3646499c7230944742d963b7b4f21f343b0c
Terry Peppers <peppers@gmail.com>
2014-06-11 23:31:47 -0500

    Removing some things I do not think I need.

--- what_am_i_running.sh
+++ what_am_i_running.sh
@@ -9,21 +9,21 @@
 RCol='\x1b[0m'    
 
 # Underline           
-URed='\x1b[4;31m';  
-UGr1='\x1b[4;92m';  
-UGr2='\x1b[4;32m';  
+URed='\x1b[4;31m'
+UGr1='\x1b[4;92m'  
+UGr2='\x1b[4;32m'  
 
-function show_versions () {
+show_versions () {
   # Node
-  NODE_VERSION="node `node --version`";
+  NODE_VERSION="node `node --version`"
   
   # Python
-  PYTHON_VERSION="python v`python -c 'import sys; print sys.version[:5]'`";
+  PYTHON_VERSION="python v`python -c 'import sys; print sys.version[:5]'`"
   
   # Ruby
-  RUBY_VERSION="`ruby --version | cut -c 1-10 | sed 's/ruby /ruby v/g'`";
+  RUBY_VERSION="`ruby --version | cut -c 1-10 | sed 's/ruby /ruby v/g'`"
   
-  echo -e "${UGr1}$NODE_VERSION${RCol} ${UGr2}$PYTHON_VERSION${RCol} ${URed}$RUBY_VERSION${RCol}";
+  echo -e "${UGr1}$NODE_VERSION${RCol} ${UGr2}$PYTHON_VERSION${RCol} ${URed}$RUBY_VERSION${RCol}"
 }
 
 show_versions
